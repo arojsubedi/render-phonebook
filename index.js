4a11f0cee73ef30dd89f8c3dcf456fcd2a8d6334
Aroj Subedi <arojsubedi@gmail.com>
2024-05-06 11:44:24 -0400

    frontend production build

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ morgan('tiny')
 
 app.use(express.json())
 app.use(cors());
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'));
 
 const persons = [
